7d95c7ae6587002c9952e9a37c0f7d748cfa9d82
Litinsci <litovchenko.73@list.ru>
2025-05-27 23:52:28 +0400

    27.05 new

--- 27.05/index.js
+++ 27.05/index.js
@@ -1,23 +1,75 @@
+const users = [
+  {
+    name: "Еблан",
+    role: "admin",
+  },
+  {
+    name: "Чепуха",
+    role: "employee",
+  },
+  {
+    name: "Чушпан",
+    role: "editor",
+  },
+  {
+    name: "Тупорылый",
+    role: "admin",
+  },
+  {
+    name: "Долбоёжик",
+    role: "employee",
+  },
+  {
+    name: "Криворукий",
+    role: "editor",
+  },
+  {
+    name: "Шлёпа",
+    role: "admin",
+  },
+  {
+    name: "Петушила",
+    role: "editor",
+  },
+  {
+    name: "Мудозвон",
+    role: "admin",
+  },
+];
+
+function splitUsers(userList, userRole) {
+  return userList.filter((element) => element.role === userRole);
+}
+
+function renderUserList(userList, event) {
+  if (event.target.classList.contains("users__button-active")) {
+    return;
+  }
+  switch (event.target.id) {
+    case "buttonAdmin":
+      console.log(splitUsers(userList, "admin"));
+      break;
+    case "buttonEditor":
+      console.log(splitUsers(userList, "editor"));
+      break;
+    default:
+      console.log(userList);
+      break;
+  }
+}
+
+function buttonToggleActive(event) {
+  const activeButton = document.querySelector(".users__button-active");
+  if (activeButton) {
+    activeButton.classList.remove("users__button-active");
+  }
+  event.target.classList.add("users__button-active");
+}
+
 const userButton = document.querySelectorAll(".users__button");
-const userCards = document.querySelectorAll(".users__card");
-const userCardsAdmin = document.querySelectorAll(".users__card-admin");
-const userCardsEditor = document.querySelectorAll(".users__card-editor");
 userButton.forEach((button) => {
   button.addEventListener("click", (event) => {
-    userButton.forEach((button) => {
-      button.classList.remove("users__button-active");
-    });
-    event.target.classList.add("users__button-active");
-    switch (event.target.innerText) {
-      case "Админы":
-        console.log(userCardsAdmin);
-        break;
-      case "Редактор":
-        console.log(userCardsEditor);
-        break;
-      default:
-        console.log(userCards);
-        break;
-    }
+    renderUserList(users, event);
+    buttonToggleActive(event);
   });
 });
